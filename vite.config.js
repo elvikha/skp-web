e89dae8d2bf8c9404da9c560d80f21ab9e6195fe
Septian Ahmad Fujianto <septianfujianto@gmail.com>
2024-11-18 20:28:59 +0700

    fix: add local ip address to local dev

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: "192.168.1.172", // Your local ip address
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
